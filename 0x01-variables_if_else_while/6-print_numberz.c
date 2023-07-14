48018b9bb982037cd4745b2a60209957447981b6
Iwebi1 <iwebiisaacanuoluwa@gmail.com>
2023-07-14 14:49:24 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,10 +7,16 @@
  */
 int main(void)
 {
-	int a;
-
-	for (a = 0; a < 10; a++)
-		putchar(a + '0');
+	putchar('0' + 0);
+	putchar('0' + 1);
+	putchar('0' + 2);
+	putchar('0' + 3);
+	putchar('0' + 4);
+	putchar('0' + 5);
+	putchar('0' + 6);
+	putchar('0' + 7);
+	putchar('0' + 8);
+	putchar('0' + 9);
 
 	putchar('\n');
 	return (0);
