42d055151c8d2b479b3c0d7c655a62ace881725a
Iwebi1 <iwebiisaacanuoluwa@gmail.com>
2023-07-14 15:22:45 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
-	char letter;
+	char c;
 
-	for (letter = 'z'; letter >= 'a'; letter--)
-		putchar(letter);
+	/* Print lowercase alphabet in reverse */
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
 
+	/* Print a new line */
 	putchar('\n');
+
 	return (0);
 }
