3d05d8facc4bb42bc716508a30d1db145f7f609d
Iwebi1 <iwebiisaacanuoluwa@gmail.com>
2023-07-18 14:36:33 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints "_putchar" followed by a new line.
+ * Return: 0 (indicating success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
